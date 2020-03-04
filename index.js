6eb46219f1999cc8173c30b321493eb4c024fb54
Moges <Moges.ash@gmail.com>
2020-03-04 22:56:45 +0100

    insert objects

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-consol.log('Hello world'); // This is my first JS code
+console.log('Hello world'); // This is my first JS code
 let name='Mosh';
-consol.log(name);
-let firstName;
+console.log(name);
+let firstName='Mosh';
+let interestRate = 0.3;
+console.log(interestRate);
+console.log(name);
+
+let person={
+    name:"Mosh",
+    age: 30
+}
+person.name="merry"
+person["name"]
+console.log(person.name)
+let selecte=["red","blue"]
+selecte[2]='green'
+console.log(selecte)
