45e5a18694876d026936511c71cf82c7ea0c5d8f
Moges <moges.ash@gmail.com>
2020-03-10 11:52:49 +0100

    MijnTrials

--- copyright.js
+++ copyright.js
@@ -5,7 +5,7 @@ const vandaag = new Date();
 const jaar=vandaag.getFullYear();
 const jaarSpan = document.getElementById('jaar');
 jaarSpan.innerText=jaar;
-console.log(jaarSpan.innerText);
+//console.log(jaarSpan.innerText);
 // korter
 document.getElementById('jaar').innerText=new Date().getFullYear();
 console.log(jaarSpan.innerText);
