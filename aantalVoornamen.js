0ca84803f74af2077f7cb7f1736e675cd5cf9639
Moges <Moges.ash@gmail.com>
2020-03-15 13:13:09 +0100

    version3

--- aantalVoornamen.js
+++ aantalVoornamen.js
@@ -1,75 +1,51 @@
 "use strict"
-const naamArray=[];
-let i=0;
-document.getElementById("Versturen").onclick=function(){
-    const li = document.createElement("li");
-    li.dataset.value="1";
-    const nieuweNaam=document.getElementById("naam").value;
+const naamArray = [];
+let i = 0;
+document.getElementById("Versturen").onclick = function () {
+
+    const nieuweNaam = document.getElementById("naam").value;
     // eerste element van naamArray
-    if(i===0){
-        arrayInvullen(naamArray,nieuweNaam)
-    } else{   //voeg de rest elementen van naamArray toe
-        let teller=0;
-        for ( const naam of naamArray) {
-            console.log(naam)
-            console.log(nieuweNaam)
-            if(naam!=null && nieuweNaam!=null){
-                if(naam.localeCompare(nieuweNaam)!==0){// 0: gelijk
-                    teller+=1;                
-                } 
+    if (i === 0) {
+        arrayInvullen(naamArray, nieuweNaam)
+    } else {   //voeg de rest elementen van naamArray toe
+        let teller = 0;
+        for (const naam of naamArray) {
+            if (naam != null && nieuweNaam != null) {
+                if (naam.localeCompare(nieuweNaam) !== 0) {// 0: gelijk
+                    teller += 1;
+                }
             };
         }
-        if(teller===naamArray.length){//nieuweNaam is uniek
+        if (teller === naamArray.length) {//nieuweNaam is uniek
             arrayInvullen(naamArray, nieuweNaam);
-        }else {
+        } else {
             naamCount(nieuweNaam)
         }
     }
-    
-    
-    document.getElementById("naam").value="";
-    document.getElementById("naam").focus(); 
-    i+=1;
+
+    document.getElementById("naam").value = "";
+    document.getElementById("naam").focus();
+    i += 1;
 
 }
 
 
-function arrayInvullen(naamArray,nieuweNaam){
-            const li = document.createElement("li");
-            li.dataset.value="1";
-            li.dataset.name=nieuweNaam;
-            const tbody=document.querySelector("tbody");
-            const tr=tbody.insertRow();
-            const td=tr.insertCell();
-            const td2=tr.insertCell();
-            
-            td.appendChild(li)
-            td2.innerText=li.dataset.value;
-            td2.id=nieuweNaam;
-            li.innerText=nieuweNaam;
-            naamArray[i]=nieuweNaam;
-}
-/*
-function naamVergelijk(naam1,naam2){
-    let count=0;
-    let gelijk;
-    if(naam1.length===naam2.length){
-        for (let volgNummer=0;volgNummer!==naam1.length;volgNummer++){
-            if(naam1[volgNummer]===naam2[volgNummer]){
-                count+=1;
-            }
-            if (naam1.length===count){
-                gelijk="true";
-            } else{
-                gelijk="false";        
-            };
-        }
-    } 
+function arrayInvullen(naamArray, nieuweNaam) {
+    const li = document.createElement("li");
+    const tbody = document.querySelector("tbody");
+    const tr = tbody.insertRow();
+    const td = tr.insertCell();
+    const td2 = tr.insertCell();
 
-    return gelijk;
+    td.appendChild(li)
+    td2.innerText = "1";
+    td2.id = nieuweNaam;
+    li.innerText = nieuweNaam;
+    naamArray.push(nieuweNaam);
+
 }
-*/
+
 function naamCount(naam) {
-        const rij=document.getElementById(naam);
-        rij.innerText=parseInt((rij.innerText), 10) + 1;   
+    const rij = document.getElementById(naam);
+    rij.innerText = parseInt((rij.innerText), 10) + 1;
 }
