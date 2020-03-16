a0f8105775a1a8a7b592e75fd4c082f41e4ef0a5
Moges <Moges.ash@gmail.com>
2020-03-16 18:27:13 +0100

    localstorage

---
+++ kleuren.js
@@ -0,0 +1,32 @@
+"use strict";
+for (const button of document.querySelectorAll("button[data-achtergrondkleur]")){
+button.onclick = function () {
+const kleur = this.dataset.achtergrondkleur; 
+localStorage.setItem("achtergrondkleur", kleur); 
+document.querySelector("body").style.backgroundColor = kleur; 
+};
+}
+
+for (const button of document.querySelectorAll("button[data-tekstkleur]")) {
+    button.onclick = function () {
+    const kleur = this.dataset.tekstkleur;
+    localStorage.setItem("tekstkleur", kleur);
+    document.querySelector("body").style.color = kleur;
+    };
+    }
+    document.getElementById("vergeet").onclick = function () {
+        //localStorage.clear()
+        localStorage.removeItem("achtergrondkleur");
+        localStorage.removeItem("tekstkleur");
+        const body = document.querySelector("body");
+        body.style.backgroundColor = ""; 
+        body.style.color = "";
+        };
+const achtergrondkleur = localStorage.getItem("achtergrondkleur"); 
+if (achtergrondkleur !== null) {
+document.querySelector("body").style.backgroundColor = achtergrondkleur; 
+}
+const tekstkleur = localStorage.getItem("tekstkleur");
+if (tekstkleur !== null) {
+document.querySelector("body").style.color = tekstkleur;
+}
